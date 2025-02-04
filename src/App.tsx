7372b956f5a46ab040a3b47e53497e12713becf3
fcorralescarfaxmx <fcorrales@carfaxmx.com.mx>
2025-02-04 15:53:54 -0600

    react-useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,47 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
-import { Button } from "./components";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  const sum = () => {
-    setCount((count) => count + 1);
-  };
-
-  return (
-    <>
-      <Button label={`Count is ${count}`} parentMethod={sum} />
-    </>
+  const consoleLoader = useCallback(
+    (loadingValue: boolean) => {
+      setLoading(loadingValue);
+      console.info(loading);
+    },
+    [loading],
   );
+
+  const fetchData = useCallback(async () => {
+    consoleLoader(true);
+    try {
+      const response = await fetch("https://api.example.com/data");
+      if (!response.ok) throw new Error("Error al obtener datos");
+      const jsonData = await response.json();
+
+      setData(jsonData);
+    } catch (error) {
+      setError(error as string);
+    } finally {
+      consoleLoader(false);
+    }
+  }, [consoleLoader]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>UPS! Hay un error: {error}</div>;
+  }
+
+  return <div>{JSON.stringify(data)}</div>;
 }
 
 export default App;
